e6198f18dd4473e1ea69b30a5131d4ab11eaaa07
Faridah <faridahdada@outlook.com>
2021-05-10 14:53:45 +0100

    added 1 5kyu

--- Python_Codewars/5kyu/Human_Readable_Time.py
+++ Python_Codewars/5kyu/Human_Readable_Time.py
@@ -1,17 +1,14 @@
-import datetime
+"""
+Write a function, which takes a non-negative integer (seconds) as input and returns the time in a human-readable format (HH:MM:SS)
 
-#
-# def make_readable():
-#     # input is seconds and return in time format 00:00:00
-#     # The maximum time never exceeds 359999 (99:59:59)
-#     # need to take in the seconds
-#     # convert it to human readable time format
-#
-#     datetime.datetime.fromtimestamp(
-#         int(seconds)
-#     ).strftime('%H:%M:%S')
+HH = hours, padded to 2 digits, range: 00 - 99
+MM = minutes, padded to 2 digits, range: 00 - 59
+SS = seconds, padded to 2 digits, range: 00 - 59
+The maximum time never exceeds 359999 (99:59:59)
+
+You can find some examples in the test fixtures.
+"""
 
 
-
-time = datetime.datetime.utcfromtimestamp(int('359999')).strftime('%H:%M:%S')
-print(time)
+def make_readable(s):
+    return '{:02}:{:02}:{:02}'.format(s / 3600, s / 60 % 60, s % 60)
